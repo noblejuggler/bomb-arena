73258b06564a8b35351e6281c10cbaaf6545fd83
noblejuggler <mindstorms.ar@gmail.com>
2017-12-19 19:03:13 -0600

    Update stage_select.js

--- client/src/game/states/stage_select.js
+++ client/src/game/states/stage_select.js
@@ -15,6 +15,7 @@ var repeatingBombTilesprite;
 var stages = [
 	{name: "Limitless Brook", thumbnailKey: "thumbnails/limitless_brook_thumbnail.png", tilemapName: "levelOne", maxPlayers: 4, size: "small"},
 	{name: "Danger Desert", thumbnailKey: "thumbnails/danger_desert_thumbnail.png", tilemapName: "levelTwo", maxPlayers: 4, size: "medium"}
+	{name: "Spookey Forest", thumbnailKey: "thumbnails/spookey_forest.png", tilemapName: "levelThree", maxPlayers: 4, size: "medium"}
 ];
 
 StageSelect.prototype = {
