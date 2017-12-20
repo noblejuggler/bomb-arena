56fc1213efec55e7202f4365319db0e534de0d17
noblejuggler <mindstorms.ar@gmail.com>
2017-12-19 20:04:04 -0600

    Update powerup_ids.js

--- common/powerup_ids.js
+++ common/powerup_ids.js
@@ -1,9 +1,9 @@
 module.exports = {
-	BOMB_STRENGTH: 10,
+	BOMB_STRENGTH: 5,
 
-	BOMB_CAPACITY: 12,
+	BOMB_CAPACITY: 6,
 
-	SPEED: 14,
+	SPEED: 7,
 
 	isAPowerup: function(id) {
 		return id === this.BOMB_STRENGTH || id === this.BOMB_CAPACITY || id === this.SPEED;
