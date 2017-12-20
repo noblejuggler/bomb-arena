06e20b4218464d770b2a02c35096139a631f48b9
noblejuggler <mindstorms.ar@gmail.com>
2017-12-19 19:28:17 -0600

    Update map_info.js

--- common/map_info.js
+++ common/map_info.js
@@ -16,6 +16,15 @@ var MapInfo = {
 		tilesetName: "tiles",
 		tilesetImage: "tiles",
 		destructibleTileId: 191
+	},
+	levelThree: {
+		spawnLocations: [{x: 2, y: 1}, {x: 13, y: 1}, {x: 2, y: 13}, {x: 13, y: 13}],
+		collisionTiles: [169, 191],
+		groundLayer: "Ground",
+		blockLayer: "Blocks",
+		tilesetName: "tiles",
+		tilesetImage: "tiles",
+		destructibleTileId: 191
 	}
 };
 
